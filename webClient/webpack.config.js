73dcfcfada30050a0f2b040950d8b53e58e6b782
snly2386 <snly2386@gmail.com>
2017-11-15 16:51:40 -0800

    add web client config

---
+++ webClient/webpack.config.js
@@ -0,0 +1,53 @@
+
+var webpack = require('webpack')
+  , path = require('path')
+  , HtmlWebpackPlugin = require('html-webpack-plugin')
+  , ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+
+const VENDOR_LIBS = [
+  'react',
+  'react-dom',
+  'react-redux',
+  'react-router',
+  'redux',
+]
+
+module.exports = {
+  entry: {
+    bundle: './src/index.js',
+    vendor: VENDOR_LIBS
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].[hash].js'
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      },
+      {
+        test: /\.s?css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('style.[contenthash].css'),
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['vendor', 'manifest']
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    })
+  ]
+};
